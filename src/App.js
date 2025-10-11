5425e45ecb931156a004094023287f743dedb7d7
Ryosuke Hayashi <bikenkobu@gmail.com>
2025-10-11 16:22:36 +0900

    課題１の実装（再）

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          課題１
+          課題１（再テスト）
         </p>
         <a
           className="App-link"
